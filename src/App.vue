976d99a8f1df841770ebbdcdf5acf87c182d34fc
adim2222 <nowak.adam2222@gmail.com>
2023-08-04 11:46:27 +0200

    added the punchline reveal button

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,23 @@
 <script setup>
   import store from "./scripts/store/store.js"
+  import { ref } from "vue";
+
+  let showPunchline = ref(false);
 </script>
 
 <template>
-  {{ store.state.setup }}
-  {{ store.state.punchline }}
+  <Suspense class="">
+    <template #default>
+      {{ store.state.setup }}
+    </template>
+    <template #fallback>
+      Loading...
+    </template>
+  </Suspense>
+  <button @click="showPunchline = true">?</button>
+  <div v-if="showPunchline" class="punchline">{{ store.state.punchline }}</div>
 </template>
 
 <style scoped>
-
+  
 </style>

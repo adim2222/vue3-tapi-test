d04a9d9fd408748191bf45da7cdb284e4eac7067
adim2222 <nowak.adam2222@gmail.com>
2023-08-04 12:39:53 +0200

    added basic styling and animation

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,43 @@
 <script setup>
-  import store from "./scripts/store/store.js"
-  import { ref } from "vue";
+import store from "./scripts/store/store.js"
+import { ref } from "vue";
 
-  let showPunchline = ref(false);
+let showPunchline = ref(false);
 </script>
 
 <template>
-  <Suspense class="">
-    <template #default>
-      {{ store.state.setup }}
-    </template>
-    <template #fallback>
-      Loading...
-    </template>
-  </Suspense>
+  <div class="setup">
+    <Suspense>
+      <template #default>
+        {{ store.state.setup }}
+      </template>
+      <template #fallback>
+        Loading...
+      </template>
+    </Suspense>
+  </div>
   <button @click="showPunchline = true">?</button>
   <div v-if="showPunchline" class="punchline">{{ store.state.punchline }}</div>
 </template>
 
 <style scoped>
-  
+  .setup {
+    text-align: center;
+    font-size: 40px;
+    margin-bottom: 10px;
+  }
+  button {
+    width: 100%;
+    font-size: 50px;
+  }
+  .punchline {
+    text-align: center;
+    font-size: 60px;
+    animation-name: entry;
+    animation-duration: 1s;
+  }
+  @keyframes entry {
+    0% {opacity: 0%; transform: translate(0px,100px);}
+    100% {opacity: 100%; transform: translate(0px,0px);}
+  }
 </style>

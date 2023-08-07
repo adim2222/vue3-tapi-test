904127b62c67044bd40b8d34a740e9c371ce1aa7
adim2222 <nowak.adam2222@gmail.com>
2023-08-07 11:06:18 +0200

    more basic styling, showPunchline var to a computed var

--- src/App.vue
+++ src/App.vue
@@ -10,4 +10,12 @@ import Interface from "./components/Interface.vue"
   * {
     font-family: "Comic Sans MS", "Comic Sans", cursive;
   }
+  body {
+    background-image: url("https://img.freepik.com/free-vector/hand-painted-watercolor-pastel-sky-background_23-2148902771.jpg");
+    background-size: cover;
+    background-repeat: no-repeat;
+  }
+  html {
+    height: 100%
+  }
 </style>
--- src/components/Interface.vue
+++ src/components/Interface.vue
@@ -1,11 +1,15 @@
 <script setup>
 import store from "../scripts/store/store.js"
 import getData from "../scripts/axios/axios.js"
+import { ref, computed } from "vue";
 
 const newJoke = () => {
     getData();
-    store.dispatch('punchlineToggle');
+    store.commit('punchlineToggle');
 };
+const showPunchline = computed(() => {
+    return store.state.showPunchline;
+});
 </script>
 
 <template>
@@ -21,8 +25,8 @@ const newJoke = () => {
             </Suspense>
         </div>
         <button @click="store.dispatch('punchlineToggle')" class="reveal-button">?</button>
-        <div v-if="store.state.showPunchline" class="punchline">{{ store.state.punchline }}</div>
-        <button v-if="store.state.showPunchline" @click="newJoke" class="next-button">Next joke!</button>
+        <div v-if="showPunchline" class="punchline">{{ store.state.punchline }}</div>
+        <button v-if="showPunchline" @click="newJoke" class="next-button">Next joke!</button>
     </div>
 </template>  
 
@@ -30,6 +34,7 @@ const newJoke = () => {
 .interface-window {
     max-width: 700px;
     margin: 0 auto;
+    text-align: center;
 }
 .setup {
     text-align: center;
@@ -41,24 +46,43 @@ const newJoke = () => {
 .reveal-button {
     width: 100%;
     font-size: 50px;
+    border-radius: 10px;
+    opacity: 60%;
+    transition: 0.5s;
+}.reveal-button:hover{
+    opacity: 100%;
 }
 .next-button {
-    width: 100%;
+    margin: 0 auto;
     font-size: 50px;
-    animation-name: entry;
+    animation-name: entry-button;
     animation-duration: 1s;
     animation-delay: 2s;
     animation-fill-mode: both;
+    border-radius: 10px;
+    width: 350px;
 }
 
 .punchline {
     text-align: center;
     font-size: 60px;
-    animation-name: entry;
+    animation-name: entry-text;
     animation-duration: 1s;
+    animation-fill-mode: both;
 }
 
-@keyframes entry {
+@keyframes entry-text {
+    0% {
+        opacity: 0%;
+        transform: translate(0px, 100px);
+    }
+
+    100% {
+        opacity: 100%;
+        transform: translate(0px, 0px);
+    }
+}
+@keyframes entry-button {
     0% {
         opacity: 0%;
         transform: translate(0px, 100px);
